3e8f527232b97f070720b003d36ea9cc9fed6cce
haileyan <cocojey02@gmail.com>
2019-01-04 15:46:08 +0900

    first

---
+++ src/com/biz/grade/dao/StudentDao.java
@@ -0,0 +1,21 @@
+package com.biz.grade.dao;
+
+import java.util.List;
+
+import com.biz.grade.vo.StudentVO;
+
+public interface StudentDao {
+	
+	public int insert(StudentVO vo);
+	
+	public List<StudentVO> selectAll();
+	public StudentVO findByNum(String st_num);
+	public List<StudentVO> findByName(String st_name);
+	
+	public int update(StudentVO vo);
+	public int delete(String st_num);
+
+
+	
+
+}
